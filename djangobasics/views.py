46a3c6aa92bb9134226e9aa4419846f2b2e13230
MohitSaini <mailtomohit2002@gmail.com>
2022-09-02 23:47:46 +0530

    Activity : Added Template folder and rendered index.html from home endpoint

--- djangobasics/views.py
+++ djangobasics/views.py
@@ -1,3 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def index(request):
-    return HttpResponse("Hello")
+    params={'name':'Mohit Saini', 'place':'Mars'}
+    return render(request, 'index.html', params)
+
+def capfirst(request):
+    return HttpResponse("captilize the first letter of the word")
